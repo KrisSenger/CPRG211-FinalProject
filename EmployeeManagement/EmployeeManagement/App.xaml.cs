695c3c3cee96e5fcee597f541c3e15eb5f23b797
KSsait <kristopher.senger@edu.sait.ca>
2024-04-09 13:05:17 -0600

    Upload basic Maui Blazor template

---
+++ EmployeeManagement/EmployeeManagement/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeManagement
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeManagement
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
