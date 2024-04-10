fb544e99599a4bccdf1a5105b2633caa3dee48e2
Michellepotran <113163108+Michellepotran@users.noreply.github.com>
2024-04-09 19:29:54 -0600

    trying to connect to database

---
+++ EmployeeManagement/EmployeeManagement/Data/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Data
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int DepartmentId { get; set; }
+        public string PositionTitle { get; set; }
+        public decimal BaseSalary { get; set; }
+    }
+
+}
---
+++ EmployeeManagement/EmployeeManagement/Services/EmployeeService.cs
@@ -0,0 +1,50 @@
+﻿using MySqlConnector;
+using System.Collections.Generic;
+using EmployeeManagement.Data;
+using Microsoft.Maui.Controls;
+// Employee service to interact with the database
+public class EmployeeService
+{
+    private readonly string connectionString; // Connection string to database
+
+    public EmployeeService()
+    {
+        MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder()
+        {
+            Server = "localhost",
+            Database = "employeemanagement",
+            UserID = "root",
+            Password = "password",
+        };
+
+        connectionString = builder.ConnectionString;
+    }
+
+    public async Task<List<Employee>> GetEmployeesAsync()
+    {
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            await connection.OpenAsync();
+            var command = new MySqlCommand("SELECT * FROM Employees", connection);
+            using (var reader = await command.ExecuteReaderAsync())
+            {
+                var employees = new List<Employee>();
+                while (await reader.ReadAsync())
+                {
+                    employees.Add(new Employee
+                    {
+                        EmployeeId = reader.GetInt32("employee_id"),
+                        FirstName = reader.GetString("first_name"),
+                        LastName = reader.GetString("last_name"),
+                        DepartmentId = reader.GetInt32("department_id"),
+                        PositionTitle = reader.GetString("position_title"),
+                        BaseSalary = reader.GetDecimal("base_salary")
+                    });
+                }
+                return employees;
+            }
+        }
+    }
+
+    
+}
